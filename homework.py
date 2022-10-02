bdf2ecb714bc400c15974b4b92682033f4d6ce0c
Никита Бачу <fair.nik@yandex.ru>
2022-10-02 17:50:35 +0400

    v3.0

--- homework.py
+++ homework.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import asdict, dataclass, fields
 
 
 @dataclass
@@ -11,7 +11,7 @@ class InfoMessage:
     speed: float
     calories: float
 
-    INFO: str = (
+    INFO = (
         'Тип тренировки: {0}; '
         'Длительность: {1:.3f} ч.; '
         'Дистанция: {2:.3f} км; '
@@ -19,18 +19,12 @@ class InfoMessage:
         'Потрачено ккал: {4:.3f}.')
 
     def get_message(self) -> str:
-        return self.INFO.format(
-            self.training_type,
-            self.duration,
-            self.distance,
-            self.speed,
-            self.calories)
+        return self.INFO.format(*asdict(self).values())
 
 
 @dataclass
 class Training:
     """Базовый класс тренировки."""
-    training_type = 'Default'
     M_IN_KM = 1000
     LEN_STEP = 0.65
     TIME_IN_MIN = 60
@@ -53,25 +47,25 @@ class Training:
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
         return InfoMessage(
-            self.training_type,
-            self.duration, self.get_distance(),
+            type(self).__name__,
+            self.duration,
+            self.get_distance(),
             self.get_mean_speed(),
             self.get_spent_calories())
 
 
 class Running(Training):
     """Тренировка: бег."""
-    training_type = 'Running'
     LEN_STEP = 0.65
-    KOEFF1 = 18
-    KOEFF2 = 20
+    SPEED_MULTIPLIER = 18
+    DELTA_MULTIPLIER = 20
 
     def get_spent_calories(self):
-        mass = self.weight
-        koeff = self.M_IN_KM
-        time = self.duration
-        first = self.KOEFF1 * self.get_mean_speed() - self.KOEFF2
-        return first * mass / koeff * (time * self.TIME_IN_MIN)
+        return ((self.SPEED_MULTIPLIER
+                 * self.get_mean_speed()
+                 - self.DELTA_MULTIPLIER)
+                * self.weight / self.M_IN_KM
+                * self.TIME_IN_MIN * self.duration)
 
 
 @dataclass
@@ -79,45 +73,41 @@ class SportsWalking(Training):
     """Тренировка: спортивная ходьба."""
     training_type = 'SportsWalking'
 
-    KOEFF1 = 0.035
-    KOEFF2 = 0.029
-    KOEFF3 = 2
+    WEIGHT_MULTIPLIER_1 = 0.035
+    WEIGHT_MULTIPLIER_2 = 0.029
 
-    action: float
-    duration: float
-    weight: float
     height: float
 
     def get_spent_calories(self):
-        first = self.KOEFF1 * self.weight
-        second = (self.get_mean_speed() ** self.KOEFF3 // self.height)
-        third = self.KOEFF2 * self.weight
-        fourth = (self.duration * self.TIME_IN_MIN)
-        return (first + second * third) * fourth
+        return ((self.WEIGHT_MULTIPLIER_1 * self.weight
+                 + (self.get_mean_speed() ** 2 // self.height)
+                 * self.WEIGHT_MULTIPLIER_2 * self.weight)
+                * (self.duration * self.TIME_IN_MIN))
 
 
 @dataclass
 class Swimming(Training):
     """Тренировка: плавание."""
-    training_type = 'Swimming'
 
     LEN_STEP = 1.38
-    KOEFF1 = 1.1
-    KOEF2 = 2
+    WEIGHT_MULTIPLIER_1 = 1.1
+    WEIGHT_MULTIPLIER_2 = 2
 
-    action: float
-    duration: float
-    weight: float
     length_pool: float
-    count_pool: float
+    count_pool: int
 
     def get_mean_speed(self):
-        first = self.length_pool * self.count_pool / self.M_IN_KM
-        second = self.duration
-        return first / second
+        return (
+            (self.length_pool
+             * self.count_pool
+             / self.M_IN_KM
+             / self.duration))
 
     def get_spent_calories(self):
-        return (self.get_mean_speed() + self.KOEFF1) * self.KOEF2 * self.weight
+        return ((self.get_mean_speed()
+                 + self.WEIGHT_MULTIPLIER_1)
+                * self.WEIGHT_MULTIPLIER_2
+                * self.weight)
 
 
 WORKOUT_TYPES = {
@@ -128,6 +118,12 @@ WORKOUT_TYPES = {
 
 def read_package(workout_type: str, data) -> Training:
     """Прочитать данные полученные от датчиков."""
+    if workout_type not in WORKOUT_TYPES.keys():
+        raise NotImplementedError('Unknown training type')
+
+    elif len(data) != len(fields(WORKOUT_TYPES[workout_type])):
+        raise KeyError('Wrong number of given parameters')
+
     return WORKOUT_TYPES[workout_type](*data)
 
 
