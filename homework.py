3e8393f29ead730dcf0999d984e56d16c38185ab
Никита Бачу <fair.nik@yandex.ru>
2022-10-02 21:03:40 +0400

    v4.0

--- homework.py
+++ homework.py
@@ -1,4 +1,4 @@
-from dataclasses import asdict, dataclass, fields
+from dataclasses import astuple, dataclass, fields
 
 
 @dataclass
@@ -19,7 +19,7 @@ class InfoMessage:
         'Потрачено ккал: {4:.3f}.')
 
     def get_message(self) -> str:
-        return self.INFO.format(*asdict(self).values())
+        return self.INFO.format(*astuple(self))
 
 
 @dataclass
@@ -27,7 +27,7 @@ class Training:
     """Базовый класс тренировки."""
     M_IN_KM = 1000
     LEN_STEP = 0.65
-    TIME_IN_MIN = 60
+    HOUR_IN_MIN = 60
 
     action: float
     duration: float
@@ -58,20 +58,19 @@ class Running(Training):
     """Тренировка: бег."""
     LEN_STEP = 0.65
     SPEED_MULTIPLIER = 18
-    DELTA_MULTIPLIER = 20
+    SPEED_SUBTRAHEND = 20
 
     def get_spent_calories(self):
         return ((self.SPEED_MULTIPLIER
                  * self.get_mean_speed()
-                 - self.DELTA_MULTIPLIER)
+                 - self.SPEED_SUBTRAHEND)
                 * self.weight / self.M_IN_KM
-                * self.TIME_IN_MIN * self.duration)
+                * self.HOUR_IN_MIN * self.duration)
 
 
 @dataclass
 class SportsWalking(Training):
     """Тренировка: спортивная ходьба."""
-    training_type = 'SportsWalking'
 
     WEIGHT_MULTIPLIER_1 = 0.035
     WEIGHT_MULTIPLIER_2 = 0.029
@@ -82,7 +81,7 @@ class SportsWalking(Training):
         return ((self.WEIGHT_MULTIPLIER_1 * self.weight
                  + (self.get_mean_speed() ** 2 // self.height)
                  * self.WEIGHT_MULTIPLIER_2 * self.weight)
-                * (self.duration * self.TIME_IN_MIN))
+                * (self.duration * self.HOUR_IN_MIN))
 
 
 @dataclass
@@ -90,8 +89,8 @@ class Swimming(Training):
     """Тренировка: плавание."""
 
     LEN_STEP = 1.38
-    WEIGHT_MULTIPLIER_1 = 1.1
-    WEIGHT_MULTIPLIER_2 = 2
+    SPEED_ADDENDUM = 1.1
+    SPEED_MULTIPLIER = 2
 
     length_pool: float
     count_pool: int
@@ -105,8 +104,8 @@ class Swimming(Training):
 
     def get_spent_calories(self):
         return ((self.get_mean_speed()
-                 + self.WEIGHT_MULTIPLIER_1)
-                * self.WEIGHT_MULTIPLIER_2
+                + self.SPEED_ADDENDUM)
+                * self.SPEED_MULTIPLIER
                 * self.weight)
 
 
@@ -115,14 +114,20 @@ WORKOUT_TYPES = {
     'RUN': Running,
     'WLK': SportsWalking}
 
+KEY_ERROR_PHRASE = 'There is no training type called {} in system.'
+TYPE_ERROR_PHRASE = ('Wrong number of given arguments. '
+                     'This class takes exactly {} arguments ({} given).')
+
 
 def read_package(workout_type: str, data) -> Training:
     """Прочитать данные полученные от датчиков."""
-    if workout_type not in WORKOUT_TYPES.keys():
-        raise NotImplementedError('Unknown training type')
+    if workout_type not in WORKOUT_TYPES:
+        raise KeyError(KEY_ERROR_PHRASE.format(workout_type))
 
-    elif len(data) != len(fields(WORKOUT_TYPES[workout_type])):
-        raise KeyError('Wrong number of given parameters')
+    if len(data) != len(fields(WORKOUT_TYPES[workout_type])):
+        raise TypeError(TYPE_ERROR_PHRASE.format(
+            len(fields(WORKOUT_TYPES[workout_type])),
+            len(data)))
 
     return WORKOUT_TYPES[workout_type](*data)
 
